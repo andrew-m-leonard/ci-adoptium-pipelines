63cd48e7895fad3cab3589838ae0dc5bd4ec7966
Andrew Leonard <andrew.m.leonard@ibm.com>
2026-07-29 10:52:48 +0100

    style(ci): add blank line in job description template
    Co-authored-by: Bob <bob@ibm.com>

--- ci/jenkins/job-dsl/openjdk_build_pipeline.groovy
+++ ci/jenkins/job-dsl/openjdk_build_pipeline.groovy
@@ -157,6 +157,7 @@ pipelineJob(jobName) {
     displayName("Build_openjdk${jdkVersion}_${variant}_${architecture}_${targetOs}")
     description("""\
         Platform-specific build pipeline for OpenJDK ${jdkVersion} (${variant}) on ${architecture}/${targetOs}.
+
         pipeline-sha:${pipelineCommitSha}""".stripIndent().trim())
 
     quietPeriod(5)
